51aa20dd079d940bdcece1cfa7dbe064b7449741
youkim <42.4.youkim@gmail.com>
2021-11-16 18:57:19 +0900

    refactor: :building_construction: split headers as well

--- lib/includes/libft.h
+++ lib/includes/libft.h
@@ -6,15 +6,15 @@
 /*   By: youkim < youkim@student.42seoul.kr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 15:18:44 by youkim            #+#    #+#             */
-/*   Updated: 2021/11/16 17:02:02 by youkim           ###   ########.fr       */
+/*   Updated: 2021/11/16 18:48:58 by youkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <stdlib.h>
 # include <stdbool.h>
+# include <stdlib.h>
 
 /*	===== Documentation =====
 	refactored libft by @youkim
@@ -35,13 +35,12 @@
 # define SUCCESS 0
 # define ERROR -1
 
-// ===== @Functions =====
-// < ystrlen.c >
-int		ystrlen(char *str);
-
 /* ===== Additional Naming Convention =====
 	[prefix]
 		y: ALL LIBFT function by @youkim!
+	[infix]
+		first 3~4 letters of directory.
+		e.g yarr_len means this file contains function about length of array
 	[suffix]
 		_m:	uses MALLOC / dynamic allocation
 			since it's the root of all memory errors
