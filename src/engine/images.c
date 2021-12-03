4a6359aae1c6d3fb81d9d5a56e415834f2424276
youkim <42.4.youkim@gmail.com>
2021-12-03 15:45:23 +0900

    fix: :ambulance: img location segfaults

--- src/engine/images.c
+++ src/engine/images.c
@@ -6,7 +6,7 @@
 /*   By: youkim < youkim@student.42seoul.kr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 19:06:15 by youkim            #+#    #+#             */
-/*   Updated: 2021/12/03 14:26:23 by youkim           ###   ########.fr       */
+/*   Updated: 2021/12/03 15:42:17 by youkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,15 @@
 
 static void	load_img(t_engine *engine, char *name)
 {
+	int		fd;
 	t_img	*img;
 	char	*path;
 
 	path = new_ystrjoin((char *[]){"img/", name, ".xpm", NULL});
+	fd = open(path, O_RDONLY);
+	if (fd < 0)
+		yerror("load_img", "Failed to open image file");
+	close(fd);
 	img = malloc(sizeof(t_img));
 	img->mlx = engine->mlx;
 	img->data = mlx_xpm_file_to_image(
